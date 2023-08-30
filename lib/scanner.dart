1f5674142dbd61c82868b4f62cafb7e029c369f3
rdsarjito <rama.dhaninursarjito@gmail.com>
2023-08-30 14:03:20 +0700

    Update Scanner

--- lib/scanner.dart
+++ lib/scanner.dart
@@ -12,9 +12,8 @@ class MyQRScanner extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Flutter Demo Home Page')),
-      body: const QRViewExample(),
+    return const Scaffold(
+      body: QRViewExample(),
     );
   }
 }
