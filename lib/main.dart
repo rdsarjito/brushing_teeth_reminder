ad7c677abf367f593ee35610c3499fd2310dee7e
rdsarjito <rama.dhaninursarjito@gmail.com>
2023-09-01 14:10:40 +0700

    Cleaning Main

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(
+      home: const MyHomePage(
         period: {}
         ),
     );
