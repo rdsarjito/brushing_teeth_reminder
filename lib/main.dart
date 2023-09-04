9ec769cc3532d49248e86be477d411c23b15d327
rdsarjito <rama.dhaninursarjito@gmail.com>
2023-09-04 20:02:07 +0700

    Add QR Generator

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:timezone/data/latest.dart' as tz;
 
 import 'package:get/get.dart';
-
 import 'Services//notifi_service.dart';
 import 'home_page.dart';
+
+import 'package:qr_flutter/qr_flutter.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
